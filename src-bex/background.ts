38cc2ced52bd07d83d9a617b9b90f1dd0b2ae61d
hsinpaohuang <hsinpaohuang@gmail.com>
2024-07-15 15:42:17 +0100

    ✨ add prototype

--- src-bex/background.ts
+++ src-bex/background.ts
@@ -16,76 +16,39 @@ chrome.action.onClicked.addListener(openExtension);
 
 declare module '@quasar/app-vite' {
   interface BexEventMap {
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    log: [{ message: string; data?: any[] }, void];
-    getTime: [never, number];
-
-    'storage.get': [{ key: string | null }, any];
-    'storage.set': [{ key: string; value: any }, any];
-    'storage.remove': [{ key: string }, any];
-    /* eslint-enable @typescript-eslint/no-explicit-any */
+    getHistory: [string[]];
   }
 }
 
+const browsingHistory = new Set<string>();
+
+function onNavigation(
+  details: chrome.webNavigation.WebNavigationFramedCallbackDetails,
+) {
+  // ignore iframes
+  if (details.frameType !== 'outermost_frame') {
+    return;
+  }
+
+  // ignore non-http requests (e.g. chrome://)
+  if (!details.url.startsWith('http')) {
+    return;
+  }
+
+  browsingHistory.add(details.url);
+}
+
 export default bexBackground((bridge /* , allActiveConnections */) => {
-  bridge.on('log', ({ data, respond }) => {
-    console.log(`[BEX] ${data.message}`, ...(data.data || []));
-    respond();
+  bridge.on('getHistory', ({ respond }) => {
+    respond(Array.from(browsingHistory));
   });
 
-  bridge.on('getTime', ({ respond }) => {
-    respond(Date.now());
-  });
+  // avoid attaching duplicate listeners every time the user opens a tab
+  const hasNavListener =
+    chrome.webNavigation.onCompleted.hasListener(onNavigation);
+  if (hasNavListener) {
+    return;
+  }
 
-  bridge.on('storage.get', ({ data, respond }) => {
-    const { key } = data;
-    if (key === null) {
-      chrome.storage.local.get(null, (items) => {
-        // Group the values up into an array to take advantage of the bridge's chunk splitting.
-        respond(Object.values(items));
-      });
-    } else {
-      chrome.storage.local.get([key], (items) => {
-        respond(items[key]);
-      });
-    }
-  });
-  // Usage:
-  // const { data } = await bridge.send('storage.get', { key: 'someKey' })
-
-  bridge.on('storage.set', ({ data, respond }) => {
-    chrome.storage.local.set({ [data.key]: data.value }, () => {
-      respond();
-    });
-  });
-  // Usage:
-  // await bridge.send('storage.set', { key: 'someKey', value: 'someValue' })
-
-  bridge.on('storage.remove', ({ data, respond }) => {
-    chrome.storage.local.remove(data.key, () => {
-      respond();
-    });
-  });
-  // Usage:
-  // await bridge.send('storage.remove', { key: 'someKey' })
-
-  /*
-  // EXAMPLES
-  // Listen to a message from the client
-  bridge.on('test', d => {
-    console.log(d)
-  })
-
-  // Send a message to the client based on something happening.
-  chrome.tabs.onCreated.addListener(tab => {
-    bridge.send('browserTabCreated', { tab })
-  })
-
-  // Send a message to the client based on something happening.
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url) {
-      bridge.send('browserTabUpdated', { tab, changeInfo })
-    }
-  })
-   */
+  chrome.webNavigation.onCompleted.addListener(onNavigation);
 });
---
+++ src/components/URLList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
+import { useQuasar } from 'quasar';
+
+const { bex } = useQuasar();
+const browsingHistory = ref<string[]>([]);
+onBeforeMount(async () => {
+  browsingHistory.value = (await bex.send('getHistory')).data;
+});
+</script>
+
+<template>
+  <div>
+    <h3>History:</h3>
+    <ul>
+      <li v-for="(url, index) in browsingHistory" :key="index">
+        {{ url }}
+      </li>
+    </ul>
+  </div>
+</template>
